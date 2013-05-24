ffea0b102dc12199a979bac9c94894524713e50c
Philipp Frenzel <philipp@frenzel.net>
2013-05-24 10:26:43 +0200

    wrong bundle name

--- yiidhtmlx/assets.php
+++ yiidhtmlx/assets.php
@@ -7,7 +7,7 @@ return array(
 			'js/dhtmlxcommon.js',
 		),
 	),
-	'chart' => array(
+	'yiidhtmlx\chart' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/dhtmlxchart.js',
@@ -17,7 +17,7 @@ return array(
 		),
 		'depends' => array('yiidhtmlx'),
 	),
-	'alert' => array(
+	'yiidhtmlx\alert' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/dhtmlxmessage.js',
