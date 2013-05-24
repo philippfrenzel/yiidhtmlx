cd700bbe8b412c07871428a376139fd5f2bf17b7
Philipp Frenzel <philipp@frenzel.net>
2013-05-24 07:29:30 +0200

    fixing asstes

--- assets.php
+++ assets.php
@@ -1,40 +1,29 @@
 <?php
 
 return array(
-	'yiiext/dhtmlx' => array(
+	'yiiext/yiidhtmlx' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/dhtmlxcommon.js',
-			'js/dhtmlxcontainer.js',
 		),
 	),
-	'yii/dhtmlx/chart' => array(
+	'yiiext/yiidhtmlx/chart' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/dhtmlxchart.js',
 		),
 		'css'=>array(
-			'css/dhtmlxchart.css',
+			'css/dhtmlxchart.css'
 		),
 		'depends' => array('yiiext/dhtmlx'),
 	),
-	'yii/dhtmlx/alert' => array(
+	'yiiext/yiidhtmlx/alert' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/dhtmlxmessage.js',
 		),
 		'css'=>array(
 			'css/dhtmlxmessage_dhx_web.css'
-		),
-		'depends' => array('yiiext/dhtmlx'),
-	),
-	'yii/dhtmlx/accordion' => array(
-		'sourcePath' => __DIR__ . '/assets',
-		'js' => array(
-			'js/dhtmlxaccordion.js',
-		),
-		'css'=>array(
-			'css/dhtmlxaccordion_dhx_web.css',
 		),
 		'depends' => array('yiiext/dhtmlx'),
 	),
