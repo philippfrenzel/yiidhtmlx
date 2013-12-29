3e76ed63b937ae4f9707d9a0ea9f72d868a65e75
Philipp Frenzel <philipp@frenzel.net>
2013-12-29 23:18:04 +0100

    update assets

--- yiidhtmlx/GridObjectAsset.php
+++ yiidhtmlx/GridObjectAsset.php
@@ -21,9 +21,7 @@ class GridObjectAsset extends AssetBundle
         'dhtmlxGrid/dhtmlxgrid.js',
         'dhtmlxGrid/dhtmlxgridcell.js',        
         'dhtmlxGrid/ext/dhtmlxgrid_filter.js',
-        'dhtmlxGrid/excells/dhtmlxgrid_excell_button.js',
         'dhtmlxGrid/excells/dhtmlxgrid_excell_link.js',
-        'dhtmlxGrid/excells/dhtmlxgrid_excell_calendar.js',
         'dhtmlxGrid/excells/dhtmlxgrid_excell_acheck.js',
     );
     public $depends = array(
